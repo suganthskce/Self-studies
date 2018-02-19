26177755305b10a29bb9411914beb98f1ec52b0a
S <ssugant@amazon.com>
2018-02-19 10:52:30 +0530

    Programs added

---
+++ Java/Programs/002 Class and Object/Student2.java
@@ -0,0 +1,11 @@
+public class Student2 {
+	private int id;
+	private String name;
+	public Student2() {
+		this.id = 2;
+		this.name = "Sudhakar";
+	}
+	public void display() {
+		System.out.println("ID = "+this.id+" Name = "+this.name);
+	}
+}
