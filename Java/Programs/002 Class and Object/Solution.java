c8b0c3b4578fcb11cf329d79ce6d3dd79f359ab0
suganth2610@gmail.com <Suganth-7122@csez.zohocorpin.com>
2018-06-28 12:39:12 +0530

    Setting up environment

--- Java/Programs/002 Class and Object/Solution.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-	public static void main(String args[]) {
-		Student1 student1 = new Student1();
-		student1.display();
-		Student2 student2 = new Student2();
-		student2.display();
-	}
-}
